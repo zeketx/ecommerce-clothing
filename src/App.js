23224b9a642575e7517d4d41ef4ebc6509dba240
Ezekiel Mauricio <ezekielmauricio@Ezekiels-MBP.satx.rr.com>
2021-01-20 22:07:27 -0600

    create react app + files for homepage

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HomePage from './homepage.component';
+
+function App() {
+  return (
+    <div className="App">
+        <HomePage/>
+    </div>
+  );
+}
+
+export default App;
